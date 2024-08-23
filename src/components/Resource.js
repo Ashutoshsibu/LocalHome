1d9e79d38ee69bf7baa9970d2dc1f06b756d4051
Ashutosh Mohapatra <89243349+Ashutoshmo@users.noreply.github.com>
2024-08-23 22:18:11 +0530

    latest code

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,24 +1,27 @@
 import Spline from '@splinetool/react-spline';
 import styled from 'styled-components';
 import Backimag from './../assets/img/background.jpg'
+import { useState } from 'react';
 const MainWraper=styled.div`
 /* background-image:url(${Backimag}) ;
  */
 background-color: rgb(227 227 227);
 background-position: center center;
 background-size: cover;
-  background-repeat: no-repeat;
+background-repeat: no-repeat;
   @media(min-width: 768px) {
-    width: 1920px;
-    height: 950px;
+    width:${(props=>props.loading? "1920px":"")};
+    height: ${(props=>props.loading? "950px":"")};;
   }
 `
 
 export default function Resource() {
+  const [loading, setLoading] = useState(true);
   return (
- <MainWraper>
+ <MainWraper loading={loading}>
       <Spline
         scene= "https://prod.spline.design/A-83MDwDe-nnDmxX/scene.splinecode"
+        onLoad={() => setLoading(false)} 
       />
       </MainWraper>
   );
